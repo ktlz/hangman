e841ec735fbc06eca54bbe3825dee420370e2ff1
ktlz <67963412+ktlz@users.noreply.github.com>
2024-01-11 12:14:33 +0200

    Add files via upload

---
+++ src/Keyboard.tsx
@@ -0,0 +1,45 @@
+const KEYS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+import styles from './Keyboard.module.css'
+
+type KeyboardProps = {
+    activeLetter: string[],
+    inactiveLetters: string[],
+    addGuessedLetter: (letter: string) => void,
+    disabled?: boolean,
+}
+export function Keyboard({disabled = false, activeLetter, inactiveLetters, addGuessedLetter}: KeyboardProps) {
+    return <div style={{display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", gap: ".5rem"}}>
+        {KEYS.map(key => {
+            const isActive = activeLetter.includes(key)
+            const isInactive = inactiveLetters.includes(key)
+           return <button onClick={() => addGuessedLetter(key)} className={`${styles.btn} ${isActive ? styles.active : ""} ${isInactive ? styles.inactive : ""}`} disabled={isInactive || isActive || disabled} key={key}>{key}</button>
+        })}
+    </div>
+}
